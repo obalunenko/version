40b4f0a65de59357d060a7c29b9664defb3c7f74
Oleg Balunenko <oleg.balunenko@gmail.com>
2025-02-14 15:44:07 +0400

    feat: Use embedded buildinfo from debug package

--- version.go
+++ version.go
@@ -3,31 +3,13 @@
 // This info generated at build time and compiled into the binary.
 //
 // Usage:
-// At your build script add following lines:
-// go install -ldflags '-X github.com/obalunenko/version.version APP_VERSION -X github.com/obalunenko/version.builddate BUILD_DATE -X github.com/obalunenko/version.commit COMMIT -X github.com/obalunenko/version.shortcommit SHORTCOMMIT -X github.com/obalunenko/version.appname APP_NAME'
-// and then build your binary
-// go build
-// Please note that all future binaries will be compiled with the embedded information unless the version package is recompiled with new values.
-//
-// Alternative is use ldflags on stage of compiling:
-// GOVERSION=$(go version | awk '{print $3;}')
-// APP=myapp
-// BIN_OUT=bin/${APP}
-// BUILDINFO_VARS_PKG=github.com/obalunenko/version
-// GO_BUILD_LDFLAGS="-s -w \
-//-X ${BUILDINFO_VARS_PKG}.version=${VERSION} \
-//-X ${BUILDINFO_VARS_PKG}.commit=${COMMIT} \
-//-X ${BUILDINFO_VARS_PKG}.shortcommit=${SHORTCOMMIT} \
-//-X ${BUILDINFO_VARS_PKG}.builddate=${DATE} \
-//-X ${BUILDINFO_VARS_PKG}.appname=${APP}" \
-//-X ${BUILDINFO_VARS_PKG}.goversion=${GOVERSION}"
-// GO_BUILD_PACKAGE="<PATH to package where main.go located>"
-
-// rm -rf ${BIN_OUT}
-//
-// go build -o ${BIN_OUT} -a -ldflags "${GO_BUILD_LDFLAGS}" "${GO_BUILD_PACKAGE}"
+// go build -o ${BIN_OUT} "${GO_BUILD_PACKAGE}"
 
 package version
+
+import (
+	"runtime/debug"
+)
 
 const unset = "unset"
 
@@ -39,6 +21,34 @@ var ( // build info
 	appname     = unset
 	goversion   = unset
 )
+
+func init() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+
+	goversion = info.GoVersion
+
+	var modified bool
+
+	for _, setting := range info.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			commit = setting.Value
+		case "vcs.time":
+			builddate = setting.Value
+		case "vcs.modified":
+			modified = true
+		}
+	}
+
+	if modified {
+		commit += "+CHANGES"
+	}
+
+	appname = info.Main.Path
+}
 
 // GetGoVersion returns the go version
 func GetGoVersion() string {
